ca7169b9859c8bb27d79e1a7b158c1399cad98ed
Wladimir008 <zol_wlad@mail.ru>
2022-04-08 16:39:42 +0300

    my

---
+++ str_format.py
@@ -0,0 +1,9 @@
+age = 26
+name = 'Swaroop'
+fam1 = 'pjkjnfhtd'
+print('Возраст {0} -- {1} лет.'.format(name, age))
+print('Почему {0} забавляется с этим Python?'.format(name))
+print('Фамилия {0} присутствует'.format(fam1))
+print('{0:.3}'.format(1/3))
+print('{0:_^11}'.format('hello'))
+print('{name} написал {book}'.format(name='Swaroop', book='A Byte of Python'))
